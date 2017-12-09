77e4a51736b77f76ee1d37b66b3a74e11e238b7c
Aditya Parmar <parmar415@gmail.com>
2017-12-09 00:39:55 -0800

    Load Folder Request

---
+++ server/src/main/java/in/adityaparmar/server/entity/request/Folder.java
@@ -0,0 +1,22 @@
+package in.adityaparmar.server.entity.request;
+
+public class Folder {
+    int userid;
+    int contentid;
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public int getContentid() {
+        return contentid;
+    }
+
+    public void setContentid(int contentid) {
+        this.contentid = contentid;
+    }
+}
